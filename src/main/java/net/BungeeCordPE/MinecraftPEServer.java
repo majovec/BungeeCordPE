85cb33b922abe4e4f224ee8ab7d0ee0c7842a4a1
Andre <andrethegamer911@gmail.com>
2016-08-12 02:21:35 -0400

    Commit revert

--- src/main/java/net/BungeeCordPE/MinecraftPEServer.java
+++ src/main/java/net/BungeeCordPE/MinecraftPEServer.java
@@ -11,27 +11,9 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-/*
- *
- * ____                               _____              _ _____  ______ 
- *|  _ \                             / ____|            | |  __ \|  ____|
- *| |_) |_   _ _ __   __ _  ___  ___| |     ___  _ __ __| | |__) | |__   
- *|  _ <| | | | '_ \ / _` |/ _ \/ _ \ |    / _ \| '__/ _` |  ___/|  __|  
- *| |_) | |_| | | | | (_| |  __/  __/ |___| (_) | | | (_| | |    | |____ 
- *|____/ \__,_|_| |_|\__, |\___|\___|\_____\___/|_|  \__,_|_|    |______|
- *                    __/ |                                              
- *                   |___/                                               
- *                                          
- *
- * This program is free software, and it's under GNU General Public License v3.0+
- * You can redistribute it and/or modify under the same license.
- *
- * @author BukkitPE Team
- * @link http://www.bukkitpe.net/
- *
- *
-*/
-
+/**
+ * A Minecraft PE Server.
+ */
 public class MinecraftPEServer implements Runnable{
     public final HashMap<String, RemoteClientSession> clientSessions = new HashMap();
     public final HashMap<String, RemoteServerSession> serverSessions = new HashMap();
@@ -61,7 +43,7 @@ public class MinecraftPEServer implements Runnable{
 
     @Override
     public void run() {
-        Thread.currentThread().setName("BungeeCordPE");
+        Thread.currentThread().setName("MCPE-Proxy");
 
         try {
             checkConfig();
@@ -75,7 +57,7 @@ public class MinecraftPEServer implements Runnable{
             config = (Map<String, Object>) yml.load(new FileReader("config.yml"));
             logPacketErrors = (boolean) config.get("logDecodeErrors");
             logChat = (boolean) config.get("logChat");
-            mainServer = new InetSocketAddress((String) (config.get("Host")), (Integer)(config.get("Listen_Port")));
+            mainServer = new InetSocketAddress((String) (config.get("remoteServer")), (Integer)(config.get("remoteServerPort")));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
